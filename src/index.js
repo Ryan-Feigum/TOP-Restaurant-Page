d346a22a90236909e3890b438cf9b9232e3be403
Ryan Feigum <23489211+Ryan-Feigum@users.noreply.github.com>
2024-12-04 14:06:35 -0600

    Added logic for home button to populate page with home page content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log("It's working!!");
+import { loadHomeModule } from './src/home_module';
+
+const homeButton = getElementById("home-btn");
+homeButton.addEventListener("click", () => {
+    loadHomeModule();
+});
